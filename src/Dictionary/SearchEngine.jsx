f3003621a846638ec4281b557e50c74c33104c76
ismat27 <adegbiteismail27@gmail.com>
2023-05-08 12:45:30 +0100

    updates

--- src/Dictionary/SearchEngine.jsx
+++ src/Dictionary/SearchEngine.jsx
@@ -21,9 +21,6 @@ const SearchEngine = () => {
             const api_url = `https://api.dictionaryapi.dev/api/v2/entries/en/${typeWord}`;
             
             const response = await axios.get(api_url);
-            const { data } = response
-            const { meanings, phonetics, phonetic} = data[0]
-            console.log(data[0]);
             setWordMeaning(response.data);
             setError("")
 
